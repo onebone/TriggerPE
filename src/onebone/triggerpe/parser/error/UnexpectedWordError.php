68c3db0b74f0547cfb0a8588b5063829676ba198
onebone <jyc00410@gmail.com>
2018-02-04 22:54:07 +0900

    Added UnexpectedWordError

---
+++ src/onebone/triggerpe/parser/error/UnexpectedWordError.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onebone
+ * Date: 2018-02-04
+ * Time: 오후 10:49
+ */
+
+namespace onebone\triggerpe\parser\error;
+
+
+class UnexpectedWordError {
+
+}
